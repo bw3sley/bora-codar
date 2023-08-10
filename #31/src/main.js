5fcaa88d79a26c76191bcdbd1e0447500930a704
Wesley Bernardes <bw3sley@gmail.com>
2023-08-10 19:10:45 -0300

    :sparkles: create audio transcription project

---
+++ #31/src/main.js
@@ -0,0 +1,11 @@
+import "./global.css";
+
+import "./styles/base.css";
+import "./styles/form.css";
+import "./styles/transcription.css";
+import "./styles/loading.css";
+import "./styles/copy.css";
+
+import "./form";
+import "./loading";
+import "./copy";
